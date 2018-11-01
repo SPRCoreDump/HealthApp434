73a19906312b678b4048acb86f2f472ffc8b16ec
Matt Bressler <matth.bressler@gmail.com>
2018-11-01 11:28:11 -0400

    Worked on the main page and search page visually (still haven't really touched the code for it)

--- app/src/main/java/edu/umd/jchao/healthapp/search.java
+++ app/src/main/java/edu/umd/jchao/healthapp/search.java
@@ -1,16 +1,13 @@
 package edu.umd.jchao.healthapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class search extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -37,22 +34,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        Button btn = (Button)findViewById(R.id.New);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, search.class));
-            }
-        });
     }
-
-
 
 }
