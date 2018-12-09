bafd0feb442b10ff541cc25b685872668eed8568
Justin Chao <justinchao20@gmail.com>
2018-12-09 14:35:13 -0500

    holy shit this took a while. Implemented custom listview Currently known bugs: 1. Exercise counts positively in the net calories when incrementing repetitions. 2. placeholder images for exercise and food (to differentiate the 2) do not show up. Should be an easy fix though. 3. Listview for some reason is not scrollable

---
+++ app/src/main/java/edu/umd/jchao/healthapp/ListItem.java
@@ -0,0 +1,61 @@
+package edu.umd.jchao.healthapp;
+
+//Base class to hold information about the property
+public class ListItem {
+
+    //property basics
+    private int streetNumber;
+    private String streetName;
+    private String name;
+    private String suburb;
+    private String state;
+    private String description;
+    private String image;
+    private Double price;
+    private int bedrooms;
+    private int bathrooms;
+    private int carspots;
+    private int calories;
+    private float amount;
+
+    //constructor
+    public ListItem(int streetNumber, String streetName, String suburb, String state, String description, Double price, String image, int bedrooms, int bathrooms, int carspots){
+
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.suburb = suburb;
+        this.state = state;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+        this.carspots = carspots;
+    }
+
+    public ListItem(String name, String description, float amount, int calories, String image){
+        this.name = name;
+        this.description = description;
+        this.calories = calories;
+        this.image = image;
+        this.amount = amount;
+    }
+
+    //getters
+    public int getStreetNumber() { return streetNumber; }
+    public String getStreetName() {return streetName; }
+    public String getSuburb() {return suburb; }
+    public String getState() {return state; }
+    public String getDescription() {return description; }
+    public Double getPrice() {return price; }
+    public String getImage() { return image; }
+    public int getBedrooms(){ return bedrooms; }
+    public int getBathrooms(){ return bathrooms; }
+    public int getCarspots(){ return carspots; }
+
+    public String getName(){return name;}
+    public int getCalories(){return calories; }
+    public float getAmount(){return amount;}
+
+    public void setAmount(float amount){this.amount = amount;}
+}
